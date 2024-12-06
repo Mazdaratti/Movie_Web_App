b4b079e8ad319d9a4f3b0148409cd75bf56b7b56
Mazdaratti <vasya.mazdaratti@googlemail.com>
2024-12-06 17:01:56 +0100

    Updated docstrings/removed unused imports

--- app.py
+++ app.py
@@ -1,30 +1,12 @@
 """
-This module sets up the Flask web application for managing users and their movie collections.
-It provides routes for displaying users, adding and deleting users, adding and updating movies
-to user collections, and handling errors. The application uses SQLite as the database,
-with all necessary operations being handled by the SQLiteDataManager.
+Main module for initializing and running the Flask application.
 
-Routes:
-    - /: Home page that displays recent movies.
-    - /users: Displays a list of all users.
-    - /add_user: Page for adding a new user.
-    - /users/<int:user_id>: Displays a user's movie collection.
-    - /delete_user/<int:user_id>: Deletes a user.
-    - /users/<int:user_id>/add_movie: Adds a movie to a user's collection.
-    - /users/<int:user_id>/update_movie/<int:user_movie_id>: Updates user-specific movie details.
-    - /users/<int:user_id>/delete_movie/<int:user_movie_id>: Deletes a movie from a user's collection.
-    - Error Handlers: Custom 404 and 500 error pages.
-
-Configuration:
-    - SQLite database URI defined in the app's config.
-    - Log errors to a rotating log file ('app.log').
-
-Logging:
-    - Error logs are saved to a rotating file 'app.log' with a max size of 10KB and 1 backup.
+This module sets up the Flask app, including configuration, routes,
+blueprints, and error handlers. It uses an SQLite database for storage
+and follows a modular structure with separate concerns for routes,
+data management, and storage.
 """
 import os
-import logging
-from logging.handlers import RotatingFileHandler
 from flask import Flask
 from routes.html_routes import create_html_route
 from routes.error_handlers import page_not_found, internal_server_error
